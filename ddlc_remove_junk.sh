1d570446b07da04963dcc0392bb037cd5bee353a
ym555 <mohamed.yoyo100@gmail.com>
2019-05-08 12:16:17 +0200

    stuff

--- ddlc_remove_junk.sh
+++ ddlc_remove_junk.sh
@@ -1,1 +1,5 @@
-cat $1 | strings | grep '^.*r$' | grep -v '^D:.*$' | grep -Ev '^h.{3}$'| grep -v '^ch\d+_.*$' | grep -v '^=.*$' | less
+# cat $1 | strings | grep '^.*r$' | grep -v '^D:.*$' | grep -Ev '^.{3}r$' | grep -v '^ch\d*.*$' | grep -v '^=.*$' | grep -v '^J(0yYK.*$' | grep -v 'NN]NN]tr' | grep -v '^WK.*$'
+# cat $1 | strings | grep '^.*r$' | grep -vE -e '^D:.*$' -e '^.{3}r$' -e 'ch\d*.*' -e '^=.*$' | less
+# removes some actual dialouge, this is really a hack until i figure out how to parse 'em
+# pipe through uniq or sort -u to remove duplicates
+cat $1 | strings | grep '^.*r$' | grep -Ev -e '^D:.*$' -e '^.{3}r$' -e '^ch\d*.*$' -e '^=.*$' -e '^J\(0yYK.*$'  -e 'NN]NN]tr' -e '^WK.*$'
