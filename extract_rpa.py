0499fcdf6f20a8324fe4eeb24786942d8b7db926
ym555 <mohamed.yoyo100@gmail.com>
2019-05-09 23:18:23 +0200

    cleanup

--- extract_rpa.py
+++ extract_rpa.py
@@ -4,28 +4,16 @@ import os
 import sys
 
 def load_rpa(file):
-    offset = int(file[8:24], 16)
     key = int(file[25:33], 16)
-    index = pickle.loads(codecs.decode(file[offset:], "zlib"))
+    index = pickle.loads(codecs.decode(file[int(file[8:24], 16):], "zlib"))
+    return {k: (offset ^ key, dlen ^ key, start) for k, v in index.items() for offset, dlen, start in v}
 
-    # why is this xor encoded lmfao
-    # TODO(ym): listcomp
-    for (k, v) in index.items():
-        # Isn't  needed for ddlc files
-        # if len(v) == 2:
-            # v = (v[0], v[1], '')
-        index[k] = [(offset ^ key, dlen ^ key, start) for offset, dlen, start in v]
-
-    return index
-
-# TODO(ym): python probably has something to remove the need to create directory, open file, copy the content, close the file all manually.
 def process_rpa(file):
-    for (k, v) in load_rpa(file).items():
+    for k, (offset, dlen, start) in load_rpa(file).items():
         os.makedirs("out/" + '/'.join(k.split("/")[0:-1]), exist_ok=True)
-        f2 = open("out/" + k, "wb")
+        with open("out/" + k, "wb") as f2:
+            f2.write(file[offset:offset+dlen+1])
 
-        offset, dlen, start = v[0]
-        f2.write(file[offset:offset+dlen+1])
-        f2.close()
-
-for i in sys.argv[1:]: process_rpa(open(i, "rb").read())
+for i in sys.argv[1:]:
+    with open(i, "rb") as x:
+        process_rpa(x.read())
--- extract_script.py
+++ extract_script.py
@@ -7,22 +7,19 @@ import zlib
 RPYC2_HEADER = "RENPY RPC2"
 def read_script(r):
     header_data = r[:1024]
-    pos = len(RPYC2_HEADER)
 
     o = bytes()
-    while pos + 12 < len(header_data):
+    for pos in range(len(RPYC2_HEADER), len(header_data), 12):
         header_slot, start, length = struct.unpack("III", header_data[pos:pos+12])
         if header_slot == 0:
             break
-        pos += 12
         o += zlib.decompress(r[start:start+length])
 
-        # TODO(ym):
-        # o += pickle.loads(zlib.decompress(f.read(length)))
+        # TODO(ym): acutally load the class files and extract the dialouge out of them/process them somehow
+        # pickle.loads(zlib.decompress(f.read(length)))
     return o
 
-os.makedirs("out_script", exists_ok=True)
+os.makedirs("out_script", exist_ok=True)
 for i in sys.argv[1:]:
-    o = open("out_script/" + i.split('/')[-1], "wb")
-    o.write(read_script(open(i, "rb").read()))
-    o.close()
+    with open(f"out_script/{i.split('/')[-1] + '.out'}", "wb") as o, open(i, "rb") as x:
+        o.write(read_script(x.read()))
